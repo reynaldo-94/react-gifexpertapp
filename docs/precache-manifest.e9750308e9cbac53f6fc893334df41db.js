33fbb335b4765f6311472e337eae8dfbee2b0523
Reynaldo Cauche <rcauche@techupperu.com>
2020-10-20 12:30:46 -0500

    docs agregado

---
+++ docs/precache-manifest.e9750308e9cbac53f6fc893334df41db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d254ecf3ad3185d7ec6f17d98fb6a87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94a75b71dd30934f6184",
+    "url": "/static/css/main.2c96eccc.chunk.css"
+  },
+  {
+    "revision": "94387244b6044698ff18",
+    "url": "/static/js/2.1d6add61.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.1d6add61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94a75b71dd30934f6184",
+    "url": "/static/js/main.a4c4d122.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
